7bb45e3e9d3161298494f6fb794959de1ca3ef12
Áron <conoren@gmail.com>
2021-09-23 13:40:35 +0200

    code reassembled

--- server.js
+++ server.js
@@ -1,85 +1,92 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const initializePassport = require('./passport-config') //Login miatt
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
+//secret miatt
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
-  
-  const express = require('express')
-  const app = express()
-  const bcrypt = require('bcrypt')
-  const passport = require('passport')
-  const flash = require('express-flash')
-  const session = require('express-session')
-  
-  const initializePassport = require('./passport-config')
-  initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-  )
-  
-  const users = []
-  
-  app.set('view-engine', 'ejs')
-  app.use(express.urlencoded({ extended: false }))
-  app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
-  
-  app.get('/', checkAuthenticated, (req, res) => {
+}
+
+//hogy mehessen embedded js is
+app.set('view-engine', 'ejs') 
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', { name: req.user.name })
-  })
+})
   
-  app.get('/login', checkNotAuthenticated, (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
-  })
+})
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+
+app.get('/eventManger', checkAuthenticated, (req,res)=>{
+  res.render('eventManger.ejs')
+})
   
-  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
-  
-  app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-  })
-  
-  app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
-  })
-  
-  app.get('/logout', (req, res) => {
-    req.logOut()
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})  
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
     res.redirect('/login')
-  })
-  
-  function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
   }
+})
   
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
-    }
-    next()
+
+app.get('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+//ha jogod van, mehet tovább. különben go login screen
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
   }
+  res.redirect('/login')
+}
+//ha autentikálva van, megy alapértelmezettre. ha nem, megy a megadottra
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
   
-  app.listen(3000)
+app.listen(3000)
---
+++ src/events.js
@@ -0,0 +1,29 @@
+const app = express()
+
+app.get('/eventApi/getAllEvents', (req, res) => {
+
+})
+
+app.get('/eventApi/getEvent:id', (req, res) => {
+
+})
+
+app.post('/eventApi', (req, res) => {
+
+})
+
+app.put('/eventApi/:id', (req, res) => {
+
+})
+
+app.delete('/eventApi/:id', (req, res) => {
+
+})
+
+app.delete('/eventApi', (req, res) => {
+
+})
+
+app.get('/eventApi?adottuseré', (req, res) => {
+
+})
