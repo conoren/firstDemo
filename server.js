8d63e7fcaa15a39f24a6ddf0746d5559ded1abaa
Áron <conoren@gmail.com>
2021-09-25 18:47:18 +0200

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
+ //big test
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
