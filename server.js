8fbb14718e20fb0c82d899923610b1ef2ed4c4c2
conoren <conoren@gmail.com>
2021-10-01 22:13:13 +0200

    refactoring start

--- server.js
+++
@@ -1,8 +0,0 @@
-require('./src/routes/routes.js')
-
-const express = require("express");
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json')
-const app = express();
-
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
